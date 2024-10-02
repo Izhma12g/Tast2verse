47aceee5642666a50055b1198de41618be58310d
Izhma <dabestgamerok@gmail.com>
2024-10-03 00:59:00 +1300

    Updated links fixed filter

--- explore.js
+++ explore.js
@@ -2,6 +2,8 @@ const mainContainer = document.querySelector('.main');
 const urlParams = new URLSearchParams(window.location.search);
 const query = urlParams.get('search');
 const bar = document.getElementById('bar');
+const recipeId = urlParams.get('id');
+init();
 
 // Load recipes from recipes.json and create tiles dynamically
 async function loadRecipes() {
@@ -36,8 +38,19 @@ async function loadRecipes() {
     }
 }
 
-// Call the loadRecipes function to populate the page with recipes
-loadRecipes();
+async function init() {
+    await loadRecipes(); // Wait for loadRecipes to complete
+
+    if (recipeId) {
+        selectCategory(document.getElementById(recipeId), true);
+    } else if (query) {
+        selectCategory(document.getElementById('all'));
+        bar.value = query;
+        search(query);
+    } else {
+        selectCategory(document.getElementById('all'));
+    }
+}
 
 // Category filter function
 function selectCategory(element, update) {
@@ -46,18 +59,18 @@ function selectCategory(element, update) {
         const selectedCategory = element.getAttribute('data-category');
         const recipes = document.querySelectorAll('.fil');
         
-        let visibleCount = 0;
+        let count = 0;
 
         recipes.forEach(recipe => {
             const recipeCategory = recipe.getAttribute('data-category');
             if (selectedCategory === 'all' || recipeCategory === selectedCategory) {
                 recipe.style.display = 'block';
-                visibleCount++;
+                count++;
             } else {
                 recipe.style.display = 'none';
             }
         });
-        updateTiles(visibleCount);
+        updateTiles(count);
     }
 
     // Update the selected filter styling
@@ -74,22 +87,22 @@ bar.addEventListener('input', function() {
 function search(sQuery) { 
     selectCategory(document.getElementById('all'));
     const tiles = document.querySelectorAll('.tile'); // Get all recipe tiles
-    let visibleCount = 0;
+    let count = 0;
     // Loop through each tile and check if it matches the search query
     tiles.forEach(tile => {
         const recipeName = tile.querySelector('p').textContent.toLowerCase(); // Get the recipe name text
         if (recipeName.includes(sQuery)) {
             tile.parentElement.style.display = 'block'; // Show tile if it matches
-            visibleCount++;
+            count++;
         } else {
             tile.parentElement.style.display = 'none'; // Hide tile if it doesn't match
         }
     });
-    updateTiles(visibleCount);
+    updateTiles(count);
 }
 
-function updateTiles(visibleCount) {
-    if (visibleCount <= 5) {
+function updateTiles(count) {
+    if (count <= 5) {
         mainContainer.style.marginBottom = '28.24vh';
     } else {
         mainContainer.style.marginBottom = '3.24vh';
